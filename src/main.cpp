39656426262c17512db608ded4778e86ccd97673
adhith <24882134+kkadhith@users.noreply.github.com>
2023-07-13 15:31:16 -0700

    Basic image editor, argument support implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
-#include "lib/debugutil.hpp"
+#include <vector>
+#include <string>
 
+#include "lib/debugutil.hpp"
+#include "lib/bitmap.h"
 
 int main(int argc, char* argv[]) {
     
-     
     if (argc < 2) {
         signal_error("Need at least one image to carve.\n");
     }
-    
+    std::string filename = argv[1];
+    Pixel rgb;
+    Bitmap image;
+    std::vector<std::vector<Pixel>> bmp;
+    image.open(filename);
+    bool validBmp = image.isImage();
+
+    if(validBmp == true) {
+        bmp = image.toPixelMatrix();
+
+        for (int i = 0; i < 50; i++) {
+            rgb = bmp[i][4];
+            rgb.red = 0;
+            rgb.green = 0;
+            bmp[i][4] = rgb;
+        }
+
+        image.fromPixelMatrix(bmp);
+        image.save("example1.bmp");
+    }
 
     return 0;
 }
