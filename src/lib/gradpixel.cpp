a17eda34ff0a2dbfc50519bc1c7bbd7816e95fb4
adhith <24882134+kkadhith@users.noreply.github.com>
2023-07-24 22:04:11 -0700

    Implemented main transform/gradient function

--- src/lib/gradpixel.cpp
+++ src/lib/gradpixel.cpp
@@ -1,5 +1,6 @@
 #include "gradpixel.h"
 
+#include <cmath>
 using namespace grp;
 
 PixelTransformer::PixelTransformer() {}
@@ -11,6 +12,25 @@ PixelTransformer::PixelTransformer(const PixelContainer &pc) {
     this->grads = std::vector<std::vector<float>>(row, std::vector<float>(column));
 }
 
-// void PixelTransformer::transform() {
+void PixelTransformer::transform() {
+    for (size_t i = 1; i < row-1; i++) {
+        for (size_t j = 1; j < column-1; j++) {
+            // auto.red, auto.green, auto.blue
+            
+            auto left = pixels[i][j-1];
+            auto right = pixels[i][j+1];
+            auto up = pixels[i-1][j];
+            auto down = pixels[i+1][j];
 
-// }
+            auto calculateOrientation = [](Pixel p1, Pixel p2) {
+                return std::pow((p1.red - p2.red), 2) + std::pow((p1.green - p2.green), 2) + std::pow((p1.blue - p2.blue), 2); 
+            };
+            int fx = calculateOrientation(right, left);
+            int fy = calculateOrientation(down, up);
+            float grad = std::sqrt(fx + fy);
+            
+            grads[i][j] = grad;
+        }
+    }
+    return;
+}
