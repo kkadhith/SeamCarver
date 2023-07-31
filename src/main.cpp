e379743f1c5128cdc1bebd473494e93210f2daa6
adhith <24882134+kkadhith@users.noreply.github.com>
2023-07-31 16:58:04 -0700

    cleaned up main, tested functions for pixel gradients

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 
 #include "lib/debugutil.hpp"
 #include "lib/bitmap.h"
+#include "lib/gradpixel.h"
 
 
 int main(int argc, char* argv[]) {
@@ -11,19 +12,22 @@ int main(int argc, char* argv[]) {
     if (argc < 2) {
         signal_error("Need at least one image to carve.\n");
     }
-    
+
     std::string filename = argv[1];
-    Pixel rgb;
     Bitmap image;
     std::vector<std::vector<Pixel>> bmp;
     image.open(filename);
+
     bool validBmp = image.isImage();
 
     if(validBmp == true) {
         bmp = image.toPixelMatrix();
-        bmp.erase(bmp.begin(), bmp.begin()+500);
+        grp::PixelTransformer tm = grp::PixelTransformer(bmp); 
+        tm.transform();
+        tm.ptp();
+        tm.ptg();
         image.fromPixelMatrix(bmp);
-        image.save("example1.bmp");
+        // image.save("example1.bmp");
     }
 
     return 0;
