10c6037f357bf736e3f8957aab41b2a9722475cb
Adhith Karthikeyan <24882134+kkadhith@users.noreply.github.com>
2025-07-24 23:12:28 -0700

    feat/cuda-imp - add GPUSeamCarver header file

---
+++ src/lib/GPUSeamCarver.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "bitmap.h"
+#include "GPUPixel.h"
+
+#include <vector>
+
+// TODO - add namespace later
+
+class GPUSeamCarver {
+    private:
+        GPUPixel *pixels;
+        float *gradients;
+        float *seams;
+
+        int* seamPath;
+
+        // TODO - Using int for simplicity. Should this be size_t?
+        // Note - width = column, height = row
+        int width;
+        int height; 
+
+    public:
+        GPUSeamCarver();
+        GPUSeamCarver(const std::vector<std::vector<Pixel>> &pixelMatrix);
+        ~GPUSeamCarver();
+
+        void calculateGradients();
+        void calculateSeams();
+        void determineSeamToBeRemoved();
+        void deleteSeam();
+        const PixelMatrix& getPixelContainer();
+};
